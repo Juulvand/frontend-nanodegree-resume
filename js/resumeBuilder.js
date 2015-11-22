826257cf1bffc7644c8a9559d9ac4803fadbb4d8
Juulvand <Juulvand@gmial.com>
2015-11-22 16:44:25 +0100

    Added Name and Role
    First commit! Added name and role, new commands:
    
    .append()
    .prepend()
    .replace()
    console.log()
    variables

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/* 	$("#main").append(STUFF);  // Add at the end
+	$("#main").prepend(STUFF;) // Add the the top
+	var funThoughts = awesomeThoughts.replace("What has to be replaced" , "Replacement");
+	console.log(STUFF);  //write in the console!!
+*/ 
+
+var formattedName = HTMLheaderName.replace("%data%", "Julia van Deursen");
+var formattedRole = HTMLheaderRole.replace("%data%", "Student");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+
+
+
+
